88847a2dc2a11ffc3f0d12756a6ed6fdfadd4a4b
jamesmorad <jamesmorad@gmail.com>
2018-05-29 09:16:13 -0700

    bringing in DAX SC Flask app from private gitlab server

---
+++ static/dax_info.js
@@ -0,0 +1,56 @@
+var dax_info = {
+
+"ITPCF": {"name": "Internal TPC Feedthrough", "owners": "Thomas", "completed": "?", "remaining": "?"},
+
+"ETPCF": {"name": "External TPC Feedthrough", "owners": "Thomas", "completed": "?", "remaining": "?"},
+
+"PMT HV": {"name": "PMT High Voltage", "owners": "Jacob", "completed": "?", "remaining": "?"},
+
+"Cathode HV": {"name": "Cathode High Voltage", "owners": "Jacob", "completed": "?", "remaining": "?"},
+
+"Anode HV":{"name": "Anode High Voltage", "owners": "Jacob", "completed": "?", "remaining": "?"},
+
+"PIN Diode Power":{"name": "PIN Diode Power", "owners": ["Jacob","Samuel"], "completed": "?", "remaining": "?"},
+
+"SiPM Power":{"name": "SiPM Power", "owners": "Ben", "completed": "?", "remaining": "?"},
+
+"PIN Diode Signal":{"name": "PIN Diode Signal", "owners": "Ben", "completed": "?", "remaining": "?"},
+
+"PMT Signal":{"name": "PMT Signal", "owners": "Jacob", "completed": "?", "remaining": "?"},
+
+"SiPM Signal":{"name": "SiPM Signal", "owners": "Ben", "completed": "?", "remaining": "?"},
+
+"Slow Control PC":{"name": "Slow Control PC", "owners": ["Nathaniel","James"], "completed": "Initial setup of the operating system, database, and slow control software is done.", "remaining": "Slow control connectivity and verification remain."},
+
+"RS232-USB":{"name": "RS232-USB Controller", "owners": ["Nathaniel","James"] , "completed": "Setup", "remaining": "Nothing"},
+
+"RS485-USB":{"name": "RS488-USB Controller", "owners": ["Nathaniel","James"], "completed": "Setup", "remaining": "Nothing"},
+
+"Ethernet Switch":{"name": "Ethernet Switch", "owners": ["Nathaniel","James"], "completed": "Setup", "remaining": "Nothing"},
+
+"PMT Signal Ext":{"name": "External PMT Signal", "owners": "James", "completed": "None", "remaining": "Create coax cable of correct size and route cable."},
+
+"PIN Diode Signal Ext":{"name": "External PIN Diode Signal", "owners": "James", "completed": "None", "remaining": "Create coax cable of correct size and route cable."},
+
+"SiPM Signal Ext":{"name": "External SiPM Signal", "owners": "James", "completed": "None", "remaining": "Create coax cable of correct size and route cable."},
+
+"Red Pitaya":{"name": "Red Pitaya", "owners": "Vik", "completed": "Created working DAQ suite for simple triggers on single board with binary file out. ", "remaining": "GUI interface for DAQ. Multi-Pitaya connection."},
+
+"ResHeater":{"name": "Resistive Heating Element", "owners": "?", "completed": "?", "remaining": "?"},
+
+"PT100":{"name": "PT-100 Temperature Element", "owners": "?", "completed": "?", "remaining": "?"},
+
+"CyroCon":{"name": "CryoCon Temperature Controller", "owners": "Jacob", "completed": "?", "remaining": "?"},
+
+"Pressure Gauge":{"name": "Pressure Transducer Gauge", "owners": "Nathaniel", "completed": "?", "remaining": "?"},
+
+"MFC":{"name": "Mass Flow Controller", "owners": "Nathaniel", "completed": "?", "remaining": "?"},
+
+"Anode HV Cont":{"name": "Anode High Voltage Control Box", "owners": ["Jacob","Nathaniel"], "completed": "?", "remaining": "?"},
+
+"Cathode HV Cont":{"name": "Cathode High Voltage Control Box", "owners": ["Jacob","Nathaniel"], "completed": "?", "remaining": "?"},
+
+"Strain Gauge":{"name": "Strain Gauge Controller", "owners": "Nathaniel", "completed": "Strain gauge reads on controller.", "remaining": "Need ethernet cabling to connect controller to PC"},
+
+"Vacuum Gauge":{"name": "Vacuum Gauge Controller", "owners": "Nathaniel", "completed": "Vacuum gauge reads on controller.", "remaining": "Need ethernet cabling to connect controller to PC"}
+}
